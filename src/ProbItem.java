0e6bad21d18e58c471020655786cb49723164c6b
Simon Asger Gjerløv-Christensen <simongjer@gmail.com>
2015-11-15 16:58:14 +0100

    Done with "Get Random Item" Started on "ShowBoard"

--- src/ProbItem.java
+++ src/ProbItem.java
@@ -1,8 +1,34 @@
+import java.util.Random;
+
 
 public class ProbItem {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //Unit Testing
 
+		ProbItem pi = new ProbItem();
+
+		for(int i=0; i < 20; i++)
+			System.out.println("Random Item: " + pi.getRndItem());
+
+		System.out.println();
+
+		int[] frequencies = new int[] {10, 4, 3, 1, 2};
+		char[] chs = new char[] {'A', 'Z', 'D', 'B', 'C'};
+
+		int[] nFreq = new int[chs.length];
+		pi = new ProbItem(frequencies, chs);
+		int n = 10_000;
+		for(int i = 0; i < n; i++) {
+			char ch = pi.getRndItem();
+			for (int iCh = 0; iCh < nFreq.length; iCh++) {
+				if (chs[iCh] == ch) {nFreq[iCh]++; break;}
+			}
+		}
+		for (int i = 0; i < nFreq.length; i++) {
+			System.out.println("Char: " + chs[i] + 
+					" Frequency: " + (double) nFreq[i] / n * 100 + '%' + 
+					"   Expected frequncy: " + (double) frequencies[i] / pi.sumFre * 100 + '%');
+		}
 	}
 
 
@@ -13,6 +39,8 @@ public class ProbItem {
 	char[] chRet;
 	int sumFre = 0;
 
+	Random random = new Random();
+
 	ProbItem() {
 		n = 3;
 		fqs = new int[n];
@@ -20,24 +48,34 @@ public class ProbItem {
 		chs[0] = 'A'; fqs[0] = 1;
 		chs[1] = 'B'; fqs[1] = 1;
 		chs[2] = 'C'; fqs[2] = 1;
-		
+
+		createChRet();
+	}
+
+	/**
+	 * @param frequencies
+	 * @param chs
+	 */
+	ProbItem(int[] frequencies, char[] chs) {
+		this.fqs = frequencies; this.chs = chs;
+		createChRet();
+	}
+
+	private void createChRet() {
 		for (int f : fqs) sumFre += f;
+
 		chRet = new char[sumFre];
-		
-		
+		int iCh = 0;
+		for (int i=0; i < fqs.length; i++)
+			for (int j=0; j < fqs[i]; j++)
+				chRet[iCh++] = chs[i];
 	}
 
-
-	ProbItem(int[] frequency, char[] chs) {
-		this.fqs = frequency; this.chs = chs;
-		for (int f : fqs) sumFre += f;
-	}
-
-
+	/** Returns a random item as if the item was draw from a sac of items (the drawn item is put into the sac again). 
+	 * @return an random item
+	 */
 	public char getRndItem() {
-
-
-		return ch;
+		return chRet[random.nextInt(chRet.length)];
 	}
 
 }
--- src/ShowBoard.java
+++ src/ShowBoard.java
@@ -3,9 +3,25 @@ public class ShowBoard {
 
 	public static void main(String[] args) {
 		int n = 8, m = 9; // n = x = columns , m = y = rows
-		char[][] b = new char[n][m];
+		char[][] board = new char[n][m];
 		
 		ProbItem pItem = new ProbItem();
+		
+		for(int r = 0; r < board[0].length; r++)
+			for(int c = 0; c < board.length; c++)
+				board[c][r] = pItem.getRndItem();
+		
+		
+		
+		
 	}
+	
+	
+	
+	public void showBoard(char[][] board) {
+		int n = board.length, m = board[0].length;
+		
+	}
+	
 
 }
