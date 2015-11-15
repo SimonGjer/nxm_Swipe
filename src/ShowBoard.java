0f235c04f4596756578f717153b273e79d04f470
Simon Asger Gjerløv-Christensen <simongjer@gmail.com>
2015-11-15 18:21:27 +0100

    no message

--- src/ShowBoard.java
+++ src/ShowBoard.java
@@ -4,29 +4,18 @@ public class ShowBoard {
 	public static void main(String[] args) { //Unit Testing
 		int n = 8, m = 9; // n = x = columns , m = y = rows
 		char[][] board = new char[n][m];
-		
+
 		ProbItem pItem = new ProbItem();
-		
+
 		for(int r = 0; r < board[0].length; r++)
 			for(int c = 0; c < board.length; c++)
 				board[c][r] = pItem.getRndItem();
-		
-		
 		showBoard(board);
-		
-		
 	}
-	
-	
-	
+
+
 	public static void showBoard(char[][] board) {
 		int n = board.length, m = board[0].length; // n = x = columns , m = y = rows
 		;;;System.out.println(n + " " + m); //**
-		
-		
-		
-		
 	}
-	
-
 }
