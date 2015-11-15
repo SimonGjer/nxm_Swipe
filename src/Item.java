7a0f6aa23db1779a54594323ed255baafd6e8b61
Simon Asger Gjerløv-Christensen <simongjer@gmail.com>
2015-11-15 21:22:15 +0100

    Improved ShowBoard

---
+++ src/Item.java
@@ -0,0 +1,81 @@
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class Item {
+
+	public static void main(String[] args) {
+		System.out.println("File: " + files.length);
+		for(File f : files) System.out.println("File: " + f.getName());
+		System.out.println(file.exists());
+	}
+
+	public static final Color cBrown = new Color(Color.YELLOW.getRed() / 3, Color.YELLOW.getGreen() / 3, Color.YELLOW.getBlue() / 3);
+	public static final Color cViolet = new Color((Color.RED.getRed() + Color.BLUE.getRed()) / 2, 0, (Color.RED.getBlue() + Color.BLUE.getRed()) / 2);
+	public static final File[] files = new File("." + "\\imgs").listFiles(); //**
+
+
+	public static final File file = new File("." + "\\imgs\\Acorn.png");
+	//	BufferedImage imgAcorg = ImageIO.read(file);
+
+	public static int nImage = 10; 
+	public static BufferedImage[] imgs = new BufferedImage[nImage];
+
+	public static Color getColor(char ch) {
+
+		switch ((int) ch % 10) {
+		case 0:	return Color.BLACK;
+		case 1:	return cBrown;
+		case 2:	return Color.RED;
+		case 3:	return Color.ORANGE;
+		case 4:	return Color.YELLOW;
+		case 5:	return Color.GREEN;
+		case 6:	return Color.BLUE;
+		case 7:	return cViolet;
+		case 8:	return Color.GRAY;
+		case 9:	return Color.WHITE;
+		default:
+			return Color.MAGENTA;
+		}
+	}
+
+	public static Image getImage(char ch) {
+		if(imgs[0] == null) readImages();
+		return imgs[ch % 8];
+	}
+
+	private static void readImages()  {
+
+		char ch = '\\';
+
+		//		BufferedImage img;
+
+		if (!new File("." + ch + "imgs" + ch + "Acorn.png").exists()) {
+			ch = '/';
+			if (!new File("." + ch + "imgs" + ch + "Acorn.png").exists()) {
+				System.out.println("Can't find files!"); return;
+			}
+		}
+		try {
+			imgs[0] = ImageIO.read(new File("." + ch + "imgs" + ch + "Acorn.png"));
+			imgs[1] = ImageIO.read(new File("." + ch + "imgs" + ch + "Apple.png"));
+			imgs[2] = ImageIO.read(new File("." + ch + "imgs" + ch + "BlueBerry.png"));
+			imgs[3] = ImageIO.read(new File("." + ch + "imgs" + ch + "Chestnut.png"));
+			imgs[4] = ImageIO.read(new File("." + ch + "imgs" + ch + "Dandelion.png"));
+			imgs[5] = ImageIO.read(new File("." + ch + "imgs" + ch + "Flower_Blue.png"));
+			imgs[6] = ImageIO.read(new File("." + ch + "imgs" + ch + "Strawberry.png"));
+			imgs[7] = ImageIO.read(new File("." + ch + "imgs" + ch + "Mushroom.png"));
+//			imgs[4] = ImageIO.read(new File("." + ch + "imgs" + ch + "Clover.png"));
+//			imgs[5] = ImageIO.read(new File("." + ch + "imgs" + ch + "Clover_Golden.png"));
+
+		}
+		catch (IOException e) {
+			System.out.println("can't read file");
+		}
+	}
+}
--- src/ShowBoard.java
+++ src/ShowBoard.java
@@ -1,21 +1,104 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 
-public class ShowBoard {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class ShowBoard extends JPanel {
+
+	public static char[][] board;
+
+	static ShowBoard sb = new ShowBoard();
+
+	public static int dI = 90; // width & height of Item
+	
 	public static void main(String[] args) { //Unit Testing
 		int n = 8, m = 9; // n = x = columns , m = y = rows
-		char[][] board = new char[n][m];
+		board = new char[n][m];
+		
+		
 
 		ProbItem pItem = new ProbItem();
 
 		for(int r = 0; r < board[0].length; r++)
 			for(int c = 0; c < board.length; c++)
 				board[c][r] = pItem.getRndItem();
+
+		JFrame frame = new JFrame(n + " x " + m + " Swipe");
+		frame.add(new ShowBoard());
+		frame.setSize(n * dI + 16, m * dI + 38); //16 & 38 border of windoww
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		showBoard(board);
 	}
 
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+
+		//		int fontSize = 48;
+		//		Font font =  new Font("Verdana", Font.BOLD, fontSize);
+		//		g2d.setFont(font);
+		//		g2d.setBackground(Color.CYAN);
+
+		g2d.setColor(Color.GRAY);
+		Color cGray = Color.GRAY;
+		Color cLGray = Color.LIGHT_GRAY;
+				g2d.fillRect(0, 0, g2d.getClip().getBounds().width, g2d.getClip().getBounds().height);
+
+		for(int r = 0; r < board[0].length; r++)
+			for(int c = 0; c < board.length; c++) {
+				char ch = board[c][r];
+				g2d.setColor((((c + r) & 1) == 0) ? cGray : cLGray);
+				g2d.fillRect(c * dI, r * dI, dI - 1, dI - 1);
+				//				g2d.drawString("" + ch, 8 + c * 50, 48 - 4 + r * 50);
+				Image img = Item.getImage(ch);
+				g2d.drawImage(img, c * dI, r * dI, (c + 1) * dI + 1, (r + 1) * dI + 1, 0, 0, 90, 90, null);
+			}
+		}
 
 	public static void showBoard(char[][] board) {
 		int n = board.length, m = board[0].length; // n = x = columns , m = y = rows
 		;;;System.out.println(n + " " + m); //**
 	}
 }
+
+
+//public class LinesDrawingExample extends JFrame {
+//
+//	public LinesDrawingExample() {
+//		super("Lines Drawing Demo");
+//
+//		setSize(600, 600);
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setLocationRelativeTo(null);
+//	}
+//
+//	void drawLines(Graphics g) {
+//		Graphics2D g2d = (Graphics2D) g;
+//
+//		g2d.drawLine(120, 50, 160, 50);
+//
+//		g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
+//
+//		g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
+//	}
+//
+//	public void paint(Graphics g) {
+//		super.paint(g);
+//		drawLines(g);
+//	}
+//
+//	public static void main(String[] args) {
+//		SwingUtilities.invokeLater(new Runnable() {
+//			@Override
+//			public void run() {
+//				new LinesDrawingExample().setVisible(true);
+//			}
+//		});
+//	}
+//}
